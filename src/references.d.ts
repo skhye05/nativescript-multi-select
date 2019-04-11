cc7c4e53eab8ecaa89b2e046b5f7b0898219c666
skhye05 <jonathan.mayunga@integrove.com>
2019-04-11 17:26:24 +0200

    updated refernce.ts

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,6 +1,2 @@
 /// <reference path="./node_modules/tns-platform-declarations/ios.d.ts" />
 /// <reference path="./node_modules/tns-platform-declarations/android.d.ts" />
-/*
-/// <reference path="./platforms/ios/typings/multi-select.ios.d.ts"/>
-/// <reference path="./platforms/android/typings/multi-select.android.d.ts"/> 
-*/
